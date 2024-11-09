f9be834fd9aa05ca4dc97f7609e1cd45c1dd839c
Mihir <mihir965@gmail.com>
2024-11-08 21:31:45 -0500

    made two files reconciled

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,1 +1,2 @@
 from .baseline_bot import *
+from .baseline_bot_debugging import *
--- Bot/baseline_bot_debugging.py
+++ Bot/baseline_bot_debugging.py
@@ -9,15 +9,6 @@ def list_open_cells(grid, n):
                 open_list.append((i,j))
     return open_list
 
-# def sensing_neighbours_blocked(grid, bot_pos):
-#     cardinality = [(0,1), (0,-1), (1,0), (-1,0), (1,1), (1,-1), (-1, 1), (-1,-1)]
-#     blocked_cells = 0
-#     for ci, cj in cardinality:
-#         test_i, test_j = bot_pos[0]+ci, bot_pos[1]+cj
-#         if grid[test_i][test_j]==1:
-#             blocked_cells+=1
-#     return blocked_cells
-
 def sensing_neighbours_blocked(grid, bot_pos):
     cardinality = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
     blocked_cells = 0
@@ -28,14 +19,6 @@ def sensing_neighbours_blocked(grid, bot_pos):
     # Debug output
     print(f"Sensing blocked neighbors at {bot_pos}: {blocked_cells} blocked")
     return blocked_cells
-
-# def update_kb_blocked(bot_kb, blocked, grid):
-#     new_bot_kb = []
-#     for i in bot_kb:
-#         blocked_cells = sensing_neighbours_blocked(grid, (i[0],i[1]))
-#         if blocked_cells==blocked:
-#             new_bot_kb.append(i)
-#     return new_bot_kb
 
 def update_kb_blocked(bot_kb, blocked, grid):
     new_bot_kb = []
@@ -81,25 +64,6 @@ def check_common_direction(bot_kb, grid, last_move_direction):
             directions[forbidden_dir] = -1 # so that it doen't come in max
     return max(directions, key=directions.get)
 
-# def attempt_movement(dir_check, grid, bot_pos):
-#     direction_offset = {
-#         'north':(-1,0),
-#         'south':(1,0),
-#         'east':(0,1),
-#         'west':(0,-1)      
-#     }
-#     move = direction_offset[dir_check]
-#     test_i, test_j = bot_pos[0]+move[0], bot_pos[1]+move[1]
-#     if grid[test_i][test_j] == 1:
-#         return False, bot_pos
-#     else:
-#         grid[bot_pos[0]][bot_pos[1]] = 0
-#         bot_pos = (test_i, test_j)
-#         if grid[test_i][test_j] == 2:
-#             print("The bot caught the rat!")
-#         grid[test_i][test_j] = 4
-#         return True, bot_pos 
-
 def attempt_movement(dir_check, grid, bot_pos):
     direction_offset = {
         'north': (-1, 0),
@@ -142,70 +106,8 @@ def update_kb_movement(move_check, dir_check, bot_kb, grid):
             if not movement_possible:
                 updated_kb_moves.append(i)
     return updated_kb_moves
-        
 
-# def main_function(grid, n, bot_pos):
-#     print(f"Original bot position that simulation knows: {bot_pos}")
-#     open_list = list_open_cells(grid, n)
-#     t = 0
-#     last_move_direction = None
-#     bot_kb = open_list
-#     print(len(bot_kb))
-#     while len(bot_kb) > 1:
-#         blocked = sensing_neighbours_blocked(grid, bot_pos) 
-#         bot_kb = update_kb_blocked(bot_kb, blocked, grid)
-#         print(len(bot_kb))
-#         dir_check = check_common_direction(bot_kb, grid, last_move_direction)
-#         print(dir_check)
-#         # We will also move the bot if possible
-#         move_check, bot_pos = attempt_movement(dir_check, grid, bot_pos)
-#         print(move_check)
-#         bot_kb = update_kb_movement(move_check, dir_check, bot_kb, grid)
-#         print(len(bot_kb))
-#         if move_check:
-#             last_move_direction = dir_check
-#         if len(bot_kb) == 0:
-#             print("Error: No possible positions remain in the knowledge base.")
-#             break
-#         t+=1
-#     if len(bot_kb)==1:
-#         print(f"Bot position is identified: {bot_kb[0]}")
-#     print(t)
-
-# def main_function(grid, n, bot_pos):
-#     print(f"Original bot position that simulation knows: {bot_pos}")
-#     open_list = list_open_cells(grid, n)
-#     t = 0
-#     last_move_direction = None
-#     bot_kb = open_list
-#     action_toggle = True  # Start with sensing on the first timestep
-    
-#     while len(bot_kb) > 1:
-#         if action_toggle:  # Sensing step
-#             blocked = sensing_neighbours_blocked(grid, bot_pos) 
-#             bot_kb = update_kb_blocked(bot_kb, blocked, grid)
-#             print(f"Sensing: Updated possible locations count: {len(bot_kb)}")
-#         else:  # Movement step
-#             dir_check = check_common_direction(bot_kb, grid, last_move_direction)
-#             print(f"Attempting movement in direction: {dir_check}")
-#             move_check, bot_pos = attempt_movement(dir_check, grid, bot_pos)
-#             bot_kb = update_kb_movement(move_check, dir_check, bot_kb, grid)
-#             print(f"Moving: Updated possible locations count: {len(bot_kb)}")
-#             if move_check:
-#                 last_move_direction = dir_check
-
-#         if len(bot_kb) == 0:
-#             print("Error: No possible positions remain in the knowledge base.")
-#             break
-        
-#         action_toggle = not action_toggle  # Alternate actions
-#         t += 1
-
-#     if len(bot_kb) == 1:
-#         print(f"Bot position is identified: {bot_kb[0]}")
-#     print(f"Total timesteps taken: {t}")
-
-def main_function(grid, n, bot_pos):
+def main_function_2(grid, n, bot_pos):
     print(f"Original bot position (known to simulation): {bot_pos}")
     open_list = list_open_cells(grid, n)
     t = 0
